36913540881f77685ab82b5d859b001b0590e1fd
Karlkim Suwanmongkol <karlkim@gmail.com>
2022-12-25 15:10:53 -0600

    jupyter startup updates

--- startup/00-misc.py
+++ startup/00-misc.py
@@ -4,6 +4,8 @@ import sys
 import time
 from pathlib import Path
 
+from typing import Dict, List, Optional, Tuple, Protocol
+
 import rich
 from rich.jupyter import print
 from rich import pretty
--- startup/01-chia.py
+++ startup/01-chia.py
@@ -6,15 +6,18 @@ from chia.rpc.rpc_client import RpcClient
 from chia.rpc.wallet_rpc_client import WalletRpcClient
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import Program, INFINITE_COST
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.types.coin_record import CoinRecord
 from chia.types.coin_spend import CoinSpend
 from chia.types.condition_opcodes import ConditionOpcode
+from chia.types.full_block import FullBlock
 from chia.types.spend_bundle import SpendBundle
 from chia.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.config import load_config
 from chia.util.default_root import DEFAULT_ROOT_PATH
 from chia.util.hash import std_hash
-from chia.util.ints import uint16
+from chia.util.ints import uint16, uint32, uint64
 from chia.util.json_util import dict_to_json_str
 from chia.wallet.puzzles import p2_delegated_puzzle_or_hidden_puzzle
 from chia.wallet.puzzles.p2_delegated_puzzle_or_hidden_puzzle import (
