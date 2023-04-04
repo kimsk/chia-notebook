f4be30a2c007c44c1882ec3a566c0d57436f1220
Karlkim Suwanmongkol <karlkim@gmail.com>
2023-04-04 16:52:25 +0700

    wip -- startup + examples

--- startup/00-misc.py
+++ startup/00-misc.py
@@ -6,8 +6,3 @@ import time
 from pathlib import Path
 
 from typing import Any, Dict, List, Optional, Set, Tuple, Protocol
-
-import rich
-from rich.jupyter import print
-from rich import pretty
-pretty.install()
---
+++ startup/03-chia-full_node_rpc.py
@@ -0,0 +1,44 @@
+from chia.types.blockchain_format.sized_bytes import bytes32
+
+# use with @with_full_node_rpc_client(self_hostname, full_node_rpc_port, chia_root, chia_config)
+async def get_blockchain_state(full_node_client):
+    blockchain_state = await full_node_client.get_blockchain_state()
+    return blockchain_state
+
+async def get_coin_record_by_name(full_node_client, name: bytes32):
+    coin_records = await full_node_client.get_coin_record_by_name(name)
+    return coin_records
+
+
+async def get_coin_records_by_parent_id(full_node_client, parent_id: bytes32):
+    coin_records = await full_node_client.get_coin_records_by_parent_ids([parent_id], include_spent_coins=True)
+    return coin_records
+
+
+async def get_coin_records_by_puzzle_hash(full_node_client, puzzle_hash: bytes32):
+    coin_records = await full_node_client.get_coin_records_by_puzzle_hash(puzzle_hash, include_spent_coins=True)
+    return coin_records
+
+
+async def get_coin_records_by_hint(full_node_client, hint: bytes32):
+    coin_records = await full_node_client.get_coin_records_by_hint(hint, include_spent_coins=True)
+    return coin_records
+
+
+async def get_coin_spend(full_node_client, coin_id: bytes32, spent_height):
+    coin_spend = await full_node_client.get_puzzle_and_solution(coin_id, spent_height)
+    return coin_spend
+
+
+async def get_coins_in_block(full_node_client, height):
+    block_records = await full_node_client.get_block_records(height, height + 1)
+    header_hash = bytes32.from_hexstr(block_records[0]["header_hash"])
+    result = await full_node_client.get_additions_and_removals(header_hash)
+    return result
+
+
+async def get_block_spends(full_node_client, height):
+    block_records = await full_node_client.get_block_records(height, height + 1)
+    header_hash = bytes32.from_hexstr(block_records[0]["header_hash"])
+    result = await full_node_client.get_block_spends(header_hash)
+    return result
